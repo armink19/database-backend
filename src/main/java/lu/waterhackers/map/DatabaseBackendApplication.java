7027bbd02313058139cf1258e485e00d2605fb40
Christian Glodt <chris@mind.lu>
2020-05-04 17:23:00 +0200

    Add configuration object to allow setting CORS origins from a property.

--- src/main/java/lu/waterhackers/map/DatabaseBackendApplication.java
+++ src/main/java/lu/waterhackers/map/DatabaseBackendApplication.java
@@ -2,6 +2,10 @@ package lu.waterhackers.map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 //@EntityScan("com.database.demo.model")
@@ -14,4 +18,16 @@ public class DatabaseBackendApplication {
         SpringApplication.run(DatabaseBackendApplication.class, args);
     }
 
+    @Value("${allowed.cors.origins:http://localhost:4200}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(allowedOrigins.split(","));
+			}
+		};
+	}
 }
